7ef50e854e3925503c69b415466d44bb17899597
Christoph Bühler <christoph@smartive.ch>
2016-07-29 15:49:59 +0200

    filter out usages

--- src/models/TsResolveFile.ts
+++ src/models/TsResolveFile.ts
@@ -1,10 +1,9 @@
 import {TsFile} from './TsFile';
 import {TsImport} from './TsImport';
 import {TsExport} from './TsExport';
-import {TsUsage} from './TsUsage';
 
 export class TsResolveFile extends TsFile {
     public imports: TsImport[] = [];
     public exports: TsExport[] = [];
-    public usages: TsUsage[] = [];
+    public usages: string[] = [];
 }
--- src/parser/TsResolveFileParser.ts
+++ src/parser/TsResolveFileParser.ts
@@ -6,6 +6,119 @@ import {TsNamedExport, TsClassExport, TsFunctionExport, TsEnumExport, TsTypeExpo
 import fs = require('fs');
 import {SyntaxKind, createSourceFile, ScriptTarget, SourceFile, ImportDeclaration, ImportEqualsDeclaration, Node, StringLiteral, Identifier, VariableStatement} from 'typescript';
 
+const usageNotAllowedParents = [
+    SyntaxKind.ImportEqualsDeclaration,
+    SyntaxKind.ImportSpecifier,
+    SyntaxKind.NamespaceImport,
+    SyntaxKind.ClassDeclaration,
+    SyntaxKind.ImportDeclaration,
+    SyntaxKind.FunctionDeclaration,
+    SyntaxKind.EnumDeclaration,
+    SyntaxKind.TypeAliasDeclaration,
+    SyntaxKind.MethodDeclaration
+];
+
+const usageAllowedIfLast = [
+    SyntaxKind.Parameter,
+    SyntaxKind.PropertyDeclaration,
+    SyntaxKind.VariableDeclaration,
+    SyntaxKind.ElementAccessExpression,
+    SyntaxKind.BinaryExpression
+];
+
+const usagePredicates = [
+    (o: Node) => usageNotAllowedParents.indexOf(o.parent.kind) === -1,
+    allowedIfLastIdentifier,
+    allowedIfPropertyDeclarationFirst
+];
+
+function allowedIfLastIdentifier(node: Node) {
+    if (usageAllowedIfLast.indexOf(node.parent.kind) === -1) {
+        return true;
+    }
+
+    let children = node.parent.getChildren().filter(o => o.kind === SyntaxKind.Identifier);
+    return children.indexOf(node) === 1;
+}
+
+function allowedIfPropertyDeclarationFirst(node: Node) {
+    if (node.parent.kind !== SyntaxKind.PropertyAccessExpression) {
+        return true;
+    }
+
+    let children = node.parent.getChildren();
+    return children.indexOf(node) === 0;
+}
+
+function importDeclaration(tsFile: TsResolveFile, node: ImportDeclaration): void {
+    let children = node.getChildren();
+
+    if (children.length < 2) {
+        return;
+    }
+
+    let libName = children.find(o => o.kind === SyntaxKind.StringLiteral) as StringLiteral;
+
+    if (children[1].kind === SyntaxKind.StringLiteral) {
+        tsFile.imports.push(new TsStringImport(libName.text));
+    } else if (children[1].kind === SyntaxKind.ImportClause && children[1].getChildAt(0).kind === SyntaxKind.NamespaceImport) {
+        let alias = children[1].getChildAt(0).getChildren().find(o => o.kind === SyntaxKind.Identifier) as Identifier;
+        tsFile.imports.push(new TsNamespaceImport(libName.text, alias.text));
+    } else if (children[1].kind === SyntaxKind.ImportClause && children[1].getChildAt(0).kind === SyntaxKind.NamedImports) {
+        let specifiers = [],
+            tsImport = new TsNamedImport(libName.text);
+        children[1]
+            .getChildAt(0) // NamedImports
+            .getChildAt(1) // SyntaxList
+            .getChildren() // All children
+            .filter(o => o.kind === SyntaxKind.ImportSpecifier)
+            .forEach(o => {
+                let children = o.getChildren();
+                let specifier = new TsResolveSpecifier(children[0].getText());
+                if (children.length === 3 && children.some(c => c.kind === SyntaxKind.AsKeyword)) {
+                    specifier.alias = children[2].getText();
+                }
+                specifiers.push(specifier);
+            });
+        tsImport.specifiers = specifiers;
+        tsFile.imports.push(tsImport);
+    }
+}
+
+function importEqualsDeclaration(tsFile: TsResolveFile, node: ImportEqualsDeclaration): void {
+    let children = node.getChildren();
+    let alias = children.find(o => o.kind === SyntaxKind.Identifier) as Identifier;
+    let libName = children
+        .find(o => o.kind === SyntaxKind.ExternalModuleReference)
+        .getChildren()
+        .find(o => o.kind === SyntaxKind.StringLiteral) as StringLiteral;
+
+    tsFile.imports.push(new TsExternalModuleImport(libName.text, alias.text));
+}
+
+function exportDeclaration(tsFile: TsResolveFile, node: Node, ctor: new (name: string) => TsNamedExport): void {
+    if (!checkIfExported(node)) {
+        return;
+    }
+    let name = node.getChildren().find(o => o.kind === SyntaxKind.Identifier) as Identifier;
+    tsFile.exports.push(new ctor(name.text));
+}
+
+function exportVariableDeclaration(tsFile: TsResolveFile, node: VariableStatement): void {
+    if (!checkIfExported(node)) {
+        return;
+    }
+    let isConst = node.declarationList.getChildren().some(o => o.kind === SyntaxKind.ConstKeyword);
+    node.declarationList.declarations.forEach(o => tsFile.exports.push(new TsVariableExport(o.name.getText(), isConst)));
+}
+
+function checkIfExported(node: Node): boolean {
+    let children = node.getChildren();
+    return children.length > 0 &&
+        children[0].kind === SyntaxKind.SyntaxList &&
+        children[0].getChildren().some(o => o.kind === SyntaxKind.ExportKeyword);
+}
+
 export class TsResolveFileParser {
     public parseFile(filePath: string | vscode.Uri): TsResolveFile {
         let files = this.parseFiles([filePath]);
@@ -22,110 +135,65 @@ export class TsResolveFileParser {
     private parseTypescript(source: SourceFile): TsResolveFile {
         let tsFile = new TsResolveFile(source.fileName);
 
-        this.rootSyntaxList(tsFile, source.getChildAt(0));
+        let syntaxList = source.getChildAt(0);
+        this.parseTypescriptImports(tsFile, syntaxList);
+        this.parseTypescriptExports(tsFile, syntaxList);
+        this.parseTypescriptUsages(tsFile, syntaxList);
 
         return tsFile;
     }
 
     private parseDefinition(source: SourceFile): any { }
 
-    private rootSyntaxList(tsFile: TsResolveFile, node: Node): void {
+    private parseTypescriptImports(tsFile: TsResolveFile, node: Node): void {
         for (let child of node.getChildren()) {
             switch (child.kind) {
                 case SyntaxKind.ImportDeclaration:
-                    this.importDeclaration(tsFile, <ImportDeclaration>child);
+                    importDeclaration(tsFile, <ImportDeclaration>child);
                     break;
                 case SyntaxKind.ImportEqualsDeclaration:
-                    this.importEqualsDeclaration(tsFile, <ImportEqualsDeclaration>child);
-                    break;
-                case SyntaxKind.ClassDeclaration:
-                    this.exportDeclaration(tsFile, child, TsClassExport);
-                    break;
-                case SyntaxKind.FunctionDeclaration:
-                    this.exportDeclaration(tsFile, child, TsFunctionExport);
-                    break;
-                case SyntaxKind.EnumDeclaration:
-                    this.exportDeclaration(tsFile, child, TsEnumExport);
-                    break;
-                case SyntaxKind.TypeAliasDeclaration:
-                    this.exportDeclaration(tsFile, child, TsTypeExport);
-                    break;
-                case SyntaxKind.InterfaceDeclaration:
-                    this.exportDeclaration(tsFile, child, TsInterfaceExport);
-                    break;
-                case SyntaxKind.VariableStatement:
-                    this.exportVariableDeclaration(tsFile, <VariableStatement>child);
+                    importEqualsDeclaration(tsFile, <ImportEqualsDeclaration>child);
                     break;
             }
         }
     }
 
-    private importDeclaration(tsFile: TsResolveFile, node: ImportDeclaration): void {
-        let children = node.getChildren();
-
-        if (children.length < 2) {
-            return;
-        }
-
-        let libName = children.find(o => o.kind === SyntaxKind.StringLiteral) as StringLiteral;
-
-        if (children[1].kind === SyntaxKind.StringLiteral) {
-            tsFile.imports.push(new TsStringImport(libName.text));
-        } else if (children[1].kind === SyntaxKind.ImportClause && children[1].getChildAt(0).kind === SyntaxKind.NamespaceImport) {
-            let alias = children[1].getChildAt(0).getChildren().find(o => o.kind === SyntaxKind.Identifier) as Identifier;
-            tsFile.imports.push(new TsNamespaceImport(libName.text, alias.text));
-        } else if (children[1].kind === SyntaxKind.ImportClause && children[1].getChildAt(0).kind === SyntaxKind.NamedImports) {
-            let specifiers = [],
-                tsImport = new TsNamedImport(libName.text);
-            children[1]
-                .getChildAt(0) // NamedImports
-                .getChildAt(1) // SyntaxList
-                .getChildren() // All children
-                .filter(o => o.kind === SyntaxKind.ImportSpecifier)
-                .forEach(o => {
-                    let children = o.getChildren();
-                    let specifier = new TsResolveSpecifier(children[0].getText());
-                    if (children.length === 3 && children.some(c => c.kind === SyntaxKind.AsKeyword)) {
-                        specifier.alias = children[2].getText();
-                    }
-                    specifiers.push(specifier);
-                });
-            tsImport.specifiers = specifiers;
-            tsFile.imports.push(tsImport);
+    private parseTypescriptExports(tsFile: TsResolveFile, node: Node): void {
+        for (let child of node.getChildren()) {
+            switch (child.kind) {
+                case SyntaxKind.ClassDeclaration:
+                    exportDeclaration(tsFile, child, TsClassExport);
+                    break;
+                case SyntaxKind.FunctionDeclaration:
+                    exportDeclaration(tsFile, child, TsFunctionExport);
+                    break;
+                case SyntaxKind.EnumDeclaration:
+                    exportDeclaration(tsFile, child, TsEnumExport);
+                    break;
+                case SyntaxKind.TypeAliasDeclaration:
+                    exportDeclaration(tsFile, child, TsTypeExport);
+                    break;
+                case SyntaxKind.InterfaceDeclaration:
+                    exportDeclaration(tsFile, child, TsInterfaceExport);
+                    break;
+                case SyntaxKind.VariableStatement:
+                    exportVariableDeclaration(tsFile, <VariableStatement>child);
+                    break;
+            }
         }
     }
 
-    private importEqualsDeclaration(tsFile: TsResolveFile, node: ImportEqualsDeclaration): void {
-        let children = node.getChildren();
-        let alias = children.find(o => o.kind === SyntaxKind.Identifier) as Identifier;
-        let libName = children
-            .find(o => o.kind === SyntaxKind.ExternalModuleReference)
-            .getChildren()
-            .find(o => o.kind === SyntaxKind.StringLiteral) as StringLiteral;
+    private parseTypescriptUsages(tsFile: TsResolveFile, node: Node): void {
+        for (let child of node.getChildren()) {
+            if (child.kind === SyntaxKind.Identifier) {
+                if (child.parent && usagePredicates.every(predicate => predicate(child))) {
+                    console.log(`${(<Identifier>child).text} - ${(<any>SyntaxKind)[child.parent.kind]}`);
+                }
+            }
 
-        tsFile.imports.push(new TsExternalModuleImport(libName.text, alias.text));
-    }
-
-    private exportDeclaration(tsFile: TsResolveFile, node: Node, ctor: new (name: string) => TsNamedExport): void {
-        if (!this.checkIfExported(node)) {
-            return;
+            if (child.getChildCount() > 0) {
+                this.parseTypescriptUsages(tsFile, child);
+            }
         }
-        let name = node.getChildren().find(o => o.kind === SyntaxKind.Identifier) as Identifier;
-        tsFile.exports.push(new ctor(name.text));
-    }
-
-    private exportVariableDeclaration(tsFile: TsResolveFile, node: VariableStatement): void {
-        if (!this.checkIfExported(node)) {
-            return;
-        }
-        let isConst = node.declarationList.getChildren().some(o => o.kind === SyntaxKind.ConstKeyword);
-        node.declarationList.declarations.forEach(o => tsFile.exports.push(new TsVariableExport(o.name.getText(), isConst)));
-    }
-
-    private checkIfExported(node: Node): boolean {
-        let children = node.getChildren();
-        return children.length > 0 &&
-            children[0].kind === SyntaxKind.SyntaxList &&
-            children[0].getChildren().some(o => o.kind === SyntaxKind.ExportKeyword);
     }
 }
