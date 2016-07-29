17d2b2e3d9bc77cf081e9e708fe62c67f1e96fd9
Christoph Bühler <christoph@smartive.ch>
2016-07-29 12:56:54 +0200

    move

---
+++ .test/resolveFileParser/exportsOnly.ts
@@ -0,0 +1,27 @@
+export class ExportedClass {
+
+}
+
+export function exportedFunction() {
+
+}
+
+export enum ExportedEnum {
+
+}
+
+export const enum ExportedConstEnum {
+
+}
+
+export var ExportedVariable = '';
+
+export const ExportedConst = '';
+
+export let ExportedLet = '';
+
+export type ExportedType = string;
+
+export interface ExportedInterface {
+
+}
