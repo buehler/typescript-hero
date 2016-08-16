4b7adf4cf4b1a9664454d4735d5edeb4757b7a21
Christoph Bühler <christoph@smartive.ch>
2016-08-16 16:06:15 +0200

    adding declaration structure

---
+++ src/models/TsDeclaration.ts
@@ -0,0 +1,53 @@
+export abstract class TsDeclaration {
+    constructor(public name: string) { }
+
+    //public abstract getItemKind(): CompletionItemKind;
+}
+
+export abstract class TsExportableDeclaration extends TsDeclaration {
+    constructor(name: string, public isExported: boolean) {
+        super(name);
+    }
+}
+
+export abstract class TsExportableCallableDeclaration extends TsExportableDeclaration {
+    public parameters: ParameterDeclaration[] = [];
+    public variables: VariableDeclaration[] = [];
+}
+
+export class InterfaceDeclaration extends TsExportableDeclaration {
+    public properties: PropertyDeclaration[] = [];
+    public methods: MethodDeclaration[] = [];
+}
+
+export class ClassDeclaration extends InterfaceDeclaration {
+    public ctor: ConstructorDeclaration;
+}
+
+export const enum PropertyVisibility {
+    Private,
+    Public,
+    Protected
+}
+
+export class PropertyDeclaration extends TsDeclaration {
+    constructor(name: string, public visibility: PropertyVisibility) {
+        super(name);
+    }
+}
+
+export class MethodDeclaration extends TsExportableCallableDeclaration { }
+
+export class FunctionDeclaration extends TsExportableCallableDeclaration { }
+
+export class ConstructorDeclaration extends TsExportableCallableDeclaration { }
+
+export class TypeAliasDeclaration extends TsExportableDeclaration { }
+
+export class EnumDeclaration extends TsExportableDeclaration { }
+
+export class VariableDeclaration extends TsExportableDeclaration { }
+
+export class ParameterDeclaration extends TsDeclaration { }
+
+export class DefaultDeclaration extends TsDeclaration { }
