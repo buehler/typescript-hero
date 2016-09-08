e6020fbdead675a87e41e98ec20cdf2c73db41d9
Christoph Bühler <christoph@smartive.ch>
2016-09-08 15:15:52 +0200

    add more test files (for later)

--- .test/foobar.ts
+++ .test/foobar.ts
@@ -1,1 +1,7 @@
-import {isString} from './resourceIndex/HelperFunctions';
+import {FancierLibraryClass} from './resourceIndex';
+import {isNumber, isString} from './resourceIndex/HelperFunctions';
+import {FancyLibraryClass} from 'fancy-library';
+
+let a = new FancierLibraryClass();
+let foo = new FancyLibraryClass();
+if(isString(1) || isNumber(1)){}
