5b7dedf30486e14ca9347870819320f45d632aaa
Christoph Bühler <christoph@smartive.ch>
2016-09-08 14:38:15 +0200

    fix actual import filter

--- src/provider/ResolveQuickPickProvider.ts
+++ src/provider/ResolveQuickPickProvider.ts
@@ -72,11 +72,11 @@ export class ResolveQuickPickProvider {
                     let parsed = parse(documentPath);
                     importedLib = workspace.asRelativePath(normalize(join(parsed.dir, importedLib)));
                 }
-                declarations = declarations.filter(o => o.from !== importedLib || !tsImport.specifiers.some(s => s.specifier === o.key));
+                declarations = declarations.filter(o => o.from.replace(/[/]?index$/, '') !== importedLib || !tsImport.specifiers.some(s => s.specifier === o.key));
             } else if (tsImport instanceof TsNamespaceImport || tsImport instanceof TsExternalModuleImport) {
                 declarations = declarations.filter(o => o.from !== tsImport.libraryName);
             } else if (tsImport instanceof TsDefaultImport) {
-                declarations = declarations.filter(o => (!(o.declaration instanceof DefaultDeclaration) || tsImport.libraryName !== o.from));
+                declarations = declarations.filter(o => (!(o.declaration instanceof DefaultDeclaration) || tsImport.libraryName !== o.from.replace(/[/]?index$/, '')));
             }
         }
 
