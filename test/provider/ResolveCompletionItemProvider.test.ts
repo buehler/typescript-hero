650c950b7b517db7724f9abe6a1675402b63ebd8
Christoph Bühler <christoph@smartive.ch>
2016-09-14 08:24:11 +0200

    add raw tests for providers

--- test/provider/ResolveCompletionItemProvider.test.ts
+++ test/provider/ResolveCompletionItemProvider.test.ts
@@ -4,6 +4,22 @@ chai.should();
 
 describe('ResolveCompletionItemProvider', () => {
 
-    it('shoud be tested');
+    it('shoud resolve to null when less then 2 characters');
+
+    it('shoud resolve to null if typing in a string');
+
+    it('shoud resolve to null if after a "." (PropertyAccess)');
+
+    it('shoud resolve to null if in a comment');
+
+    it('shoud resolve to null if writing an import');
+
+    it('shoud create a completion list');
+
+    it('shoud add an insert text edit if import would be new');
+
+    it('shoud add a replace text edit if import will be updated with new specifier');
+
+    it('shoud add no text edit if import is already imported');
 
 });
--- test/provider/ResolveQuickPickProvider.test.ts
+++ test/provider/ResolveQuickPickProvider.test.ts
@@ -4,6 +4,22 @@ chai.should();
 
 describe('ResolveQuickPickProvider', () => {
 
-    it('shoud be tested');
+    it('shoud show all possible declarations');
+
+    it('shoud order own files before typings / node modules');
+
+    it('shoud order the declarations by name');
+
+    it('shoud filter already imported named symbols');
+
+    it('shoud filter already imported modules / namespaces');
+
+    it('shoud filter already imported default imports');
+
+    it('shoud resolve to 0 items for non found symbol');
+
+    it('shoud find a declaration for a symbol');
+
+    it('shoud possibly find multiple declarations for a symbol');
 
 });
