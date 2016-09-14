af32907c7f802dc16604cd56ce30d7842abeca76
Christoph Bühler <christoph@smartive.ch>
2016-09-14 08:32:51 +0200

    raw extension tests

--- test/extensions/ResolveExtension.test.ts
+++ test/extensions/ResolveExtension.test.ts
@@ -4,6 +4,34 @@ chai.should();
 
 describe('ResolveExtension', () => {
 
-    it('shoud be tested');
+    describe('addImportToDocument', () => {
+
+        it('shoud write a module / namespace import correctly');
+
+        it('shoud write a named import correctly');
+
+        it('shoud update a named import correcty');
+
+        it('shoud use the correct relative path');
+
+        it('shoud only use forward slashes');
+
+        it('shoud use ./ for same directory files');
+
+        it('shoud remove /index from ../index.ts files');
+
+    });
+
+    describe('organizeImports', () => {
+
+        it('shoud remove unused imports');
+
+        it('shoud order string imports to the top');
+
+        it('shoud order libraries by name');
+
+        it('shoud order specifiers by name');
+
+    });
 
 });
