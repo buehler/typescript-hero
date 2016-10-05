e440e722def90331bdd252d77ac76b3ec0ea02b9
Christoph Bühler <christoph@smartive.ch>
2016-10-05 21:23:48 +0200

    toggle restarter on statusbar item click

--- src/extensions/RestartDebuggerExtension.ts
+++ src/extensions/RestartDebuggerExtension.ts
@@ -4,12 +4,13 @@ import {TshCommand} from '../models/TshCommand';
 import {Logger, LoggerFactory} from '../utilities/Logger';
 import {BaseExtension} from './BaseExtension';
 import {inject, injectable} from 'inversify';
-import {commands, ExtensionContext, FileSystemWatcher, workspace} from 'vscode';
+import {commands, ExtensionContext, FileSystemWatcher, StatusBarAlignment, window, workspace} from 'vscode';
 
 const DEBOUNCE = 1500;
 
 @injectable()
 export class RestartDebuggerExtension extends BaseExtension {
+    private statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left, 3);
     private fileWatcher: FileSystemWatcher;
     private logger: Logger;
     private restartCall: number;
@@ -38,6 +39,13 @@ export class RestartDebuggerExtension extends BaseExtension {
 
     public initialize(context: ExtensionContext): void {
         this.configure();
+        context.subscriptions.push(commands.registerCommand('typescriptHero.restartDebugger.toggle', () => {
+            this.active = !this.active;
+            this.configure();
+        }));
+        context.subscriptions.push(this.statusBarItem);
+        this.statusBarItem.command = 'typescriptHero.restartDebugger.toggle';
+        this.statusBarItem.show();
         this.logger.info('Initialized.');
     }
 
@@ -57,10 +65,14 @@ export class RestartDebuggerExtension extends BaseExtension {
             this.fileWatcher.onDidChange(() => this.restartDebugger());
             this.fileWatcher.onDidCreate(() => this.restartDebugger());
             this.fileWatcher.onDidDelete(() => this.restartDebugger());
+            this.statusBarItem.text = '$(pulse) $(check)';
+            this.statusBarItem.tooltip = 'The restart is currently active. Click to deactivate.';
         } else if (!this.active && this.fileWatcher) {
             this.fileWatcher.dispose();
             this.fileWatcher = null;
             this.logger.info(`Deactivated.`);
+            this.statusBarItem.text = '$(pulse) $(x)';
+            this.statusBarItem.tooltip = 'The restart is currently deactivated. Click to deactivate.';
         }
     }
 
