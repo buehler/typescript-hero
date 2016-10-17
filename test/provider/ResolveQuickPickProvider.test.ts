04e64c0e1082edcdb2a91b9017c0ed85ccb104fc
Christoph Bühler <christoph@smartive.ch>
2016-10-17 17:54:11 +0200

    refactor tests

--- test/provider/ResolveQuickPickProvider.test.ts
+++ test/provider/ResolveQuickPickProvider.test.ts
@@ -1,8 +1,8 @@
-import {ResolveQuickPickItem} from '../../src/models/QuickPickItems';
-import {Injector} from '../../src/IoC';
-import {ResolveQuickPickProvider} from '../../src/provider/ResolveQuickPickProvider';
+import { ResolveQuickPickItem } from '../../src/models/QuickPickItems';
+import { Injector } from '../../src/IoC';
+import { ResolveQuickPickProvider } from '../../src/provider/ResolveQuickPickProvider';
 import * as chai from 'chai';
-import {join} from 'path';
+import { join } from 'path';
 import * as vscode from 'vscode';
 
 chai.should();
@@ -19,122 +19,101 @@ describe('ResolveQuickPickProvider', () => {
 
     describe('addImport', () => {
 
-        it('shoud show all possible declarations', done => {
+        it('shoud show all possible declarations', async done => {
             let file = join(process.cwd(), '.test/foobar.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        items.should.be.an('array').with.length.of.at.least(5);
-                        ['ExportAlias', 'MyClass', 'isString', 'FancyLibraryClass', 'raw'].every(label => items.some(item => item.label === label)).should.be.true;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                items.should.be.an('array').with.length.of.at.least(5);
+                ['ExportAlias', 'MyClass', 'isString', 'FancyLibraryClass', 'raw'].every(label => items.some(item => item.label === label)).should.be.true;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud order own files before typings / node modules', done => {
+        it('shoud order own files before typings / node modules', async done => {
             let file = join(process.cwd(), '.test/foobar.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        for (let x = 0; x < 5; x++) {
-                            items[x].description.startsWith('/').should.be.true;
-                        }
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                for (let x = 0; x < 5; x++) {
+                    items[x].description.startsWith('/').should.be.true;
+                }
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud order the declarations by name', done => {
+        it('shoud order the declarations by name', async done => {
             let file = join(process.cwd(), '.test/foobar.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        (items[0].label < items[1].label).should.be.true;
-                        (items[1].label < items[2].label).should.be.true;
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                (items[0].label < items[1].label).should.be.true;
+                (items[1].label < items[2].label).should.be.true;
 
-                        (items[13].label < items[14].label).should.be.true;
-                        (items[14].label < items[15].label).should.be.true;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+                (items[13].label < items[14].label).should.be.true;
+                (items[14].label < items[15].label).should.be.true;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud filter already imported named symbols', done => {
+        it('shoud filter already imported named symbols', async done => {
             let file = join(process.cwd(), '.test/quickPickProvider/alreadyNamedImport.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        (items.some(o => o.label === 'MyClass')).should.be.false;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                (items.some(o => o.label === 'MyClass')).should.be.false;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud filter already imported modules / namespaces', done => {
+        it('shoud filter already imported modules / namespaces', async done => {
             let file = join(process.cwd(), '.test/quickPickProvider/alreadyNamespaceImport.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        (items.some(o => o.label === 'raw')).should.be.false;
-                        (items.some(o => o.label === 'json')).should.be.false;
-                        (items.some(o => o.label === 'text')).should.be.false;
-                        (items.some(o => o.label === 'urlencoded')).should.be.false;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                (items.some(o => o.label === 'raw')).should.be.false;
+                (items.some(o => o.label === 'json')).should.be.false;
+                (items.some(o => o.label === 'text')).should.be.false;
+                (items.some(o => o.label === 'urlencoded')).should.be.false;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud filter already imported external module reference', done => {
+        it('shoud filter already imported external module reference', async done => {
             let file = join(process.cwd(), '.test/quickPickProvider/alreadyExternalModuleImport.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        (items.some(o => o.label === 'raw')).should.be.false;
-                        (items.some(o => o.label === 'json')).should.be.false;
-                        (items.some(o => o.label === 'text')).should.be.false;
-                        (items.some(o => o.label === 'urlencoded')).should.be.false;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                (items.some(o => o.label === 'raw')).should.be.false;
+                (items.some(o => o.label === 'json')).should.be.false;
+                (items.some(o => o.label === 'text')).should.be.false;
+                (items.some(o => o.label === 'urlencoded')).should.be.false;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud filter already imported default imports', done => {
+        it('shoud filter already imported default imports', async done => {
             let file = join(process.cwd(), '.test/defaultExport/codefile.ts');
-            vscode.workspace
-                .openTextDocument(file)
-                .then(openDocument => provider.buildQuickPickList(openDocument))
-                .then(items => {
-                    try {
-                        (items.some(o => o.label === 'myDefaultExportedFunction')).should.be.false;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                });
+            try {
+                let doc = await vscode.workspace.openTextDocument(file),
+                    items = await provider.buildQuickPickList(doc);
+                (items.some(o => o.label === 'myDefaultExportedFunction')).should.be.false;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
     });
@@ -144,47 +123,41 @@ describe('ResolveQuickPickProvider', () => {
         const file = join(process.cwd(), '.test/quickPickProvider/importUnderTheCursor.ts');
         let document: vscode.TextDocument;
 
-        before(() => vscode.workspace.openTextDocument(file).then(doc => document = doc));
-
-        it('shoud resolve to 0 items for non found symbol', done => {
-            provider.buildQuickPickList(document, 'Fooo')
-                .then(items => {
-                    try {
-                        items.should.be.an('array').with.lengthOf(0);
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                })
-                .catch(done);
+        before(async done => {
+            document = await vscode.workspace.openTextDocument(file);
+            done();
         });
 
-        it('shoud find a declaration for a symbol', done => {
-            provider.buildQuickPickList(document, 'MyCla')
-                .then(items => {
-                    try {
-                        items.should.be.an('array').with.lengthOf(1);
-                        items[0].label.should.equal('MyClass');
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                })
-                .catch(done);
+        it('shoud resolve to 0 items for non found symbol', async done => {
+            try {
+                let items = await provider.buildQuickPickList(document, 'Fooo');
+                items.should.be.an('array').with.lengthOf(0);
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
-        it('shoud possibly find multiple declarations for a symbol', done => {
-            provider.buildQuickPickList(document, 'Fancier')
-                .then(items => {
-                    try {
-                        items.should.be.an('array').with.lengthOf(2);
-                        (items.every(o => o.label === 'FancierLibraryClass')).should.be.true;
-                        done();
-                    } catch (e) {
-                        done(e);
-                    }
-                })
-                .catch(done);
+        it('shoud find a declaration for a symbol', async done => {
+            try {
+                let items = await provider.buildQuickPickList(document, 'MyCla');
+                items.should.be.an('array').with.lengthOf(1);
+                items[0].label.should.equal('MyClass');
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
+
+        it('shoud possibly find multiple declarations for a symbol', async done => {
+            try {
+                let items = await provider.buildQuickPickList(document, 'Fancier');
+                items.should.be.an('array').with.lengthOf(2);
+                (items.every(o => o.label === 'FancierLibraryClass')).should.be.true;
+                done();
+            } catch (e) {
+                done(e);
+            }
         });
 
     });
