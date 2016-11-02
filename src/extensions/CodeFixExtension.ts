e2febf7cec993487546416d5fe056f47f0dac3bb
Christoph Buehler <christoph.buehler@bluewin.ch>
2016-11-02 20:09:27 +0100

    adding possibility to take a code action on missing imports

--- src/extensions/CodeFixExtension.ts
+++ src/extensions/CodeFixExtension.ts
@@ -1,10 +1,19 @@
+import { CodeAction } from '../models/CodeAction';
 import { TypescriptCodeActionProvider } from '../provider/TypescriptCodeActionProvider';
 import { CommandQuickPickItem } from '../models/QuickPickItems';
 import { Logger, LoggerFactory } from '../utilities/Logger';
 import { BaseExtension } from './BaseExtension';
 import { inject, injectable } from 'inversify';
-import { commands, ExtensionContext, languages } from 'vscode';
+import { commands, ExtensionContext, languages, window } from 'vscode';
 
+/**
+ * Extension that helps the user fix problems in the code.
+ * As an example, if the user copy pastes code and does not have the imports, this part should help with that.
+ * 
+ * @export
+ * @class CodeFixExtension
+ * @extends {BaseExtension}
+ */
 @injectable()
 export class CodeFixExtension extends BaseExtension {
     private logger: Logger;
@@ -24,8 +33,10 @@ export class CodeFixExtension extends BaseExtension {
     }
 
     public initialize(context: ExtensionContext): void {
-        context.subscriptions.push(commands.registerCommand('typescriptHero.codeFix.executeCodeAction',
-            (codeAction: any) => this.executeCodeAction(codeAction)));
+        context.subscriptions.push(commands.registerCommand(
+            'typescriptHero.codeFix.executeCodeAction',
+            (codeAction: CodeAction) => this.executeCodeAction(codeAction)
+        ));
         context.subscriptions.push(languages.registerCodeActionsProvider('typescript', this.codeActionProvider));
         this.logger.info('Initialized.');
     }
@@ -34,8 +45,18 @@ export class CodeFixExtension extends BaseExtension {
         this.logger.info('Dispose called.');
     }
 
-    private executeCodeAction(codeAction: any): void {
-        console.log(codeAction);
+    /**
+     * Executes a code action. If the result is false, a warning is shown.
+     * 
+     * @private
+     * @param {CodeAction} codeAction
+     * @returns {Promise<void>}
+     * 
+     * @memberOf CodeFixExtension
+     */
+    private async executeCodeAction(codeAction: CodeAction): Promise<void> {
+        if (!await codeAction.execute()) {
+            window.showWarningMessage('The provided code action could not complete. Please see the logs.');
+        }
     }
-
 }
--- src/provider/TypescriptCodeActionProvider.ts
+++ src/provider/TypescriptCodeActionProvider.ts
@@ -1,5 +1,16 @@
-import { injectable } from 'inversify';
-import { CancellationToken, CodeActionContext, CodeActionProvider, Command, Range, TextDocument } from 'vscode';
+import { AddImportCodeAction, CodeAction } from '../models/CodeAction';
+import { ResolveIndex } from '../caches/ResolveIndex';
+import { Logger, LoggerFactory } from '../utilities/Logger';
+import { inject, injectable } from 'inversify';
+import {
+    CancellationToken,
+    CodeActionContext,
+    CodeActionProvider,
+    Command,
+    Diagnostic,
+    Range,
+    TextDocument
+} from 'vscode';
 
 /**
  * Provider instance that is responsible for the "light bulb" feature.
@@ -12,7 +23,14 @@ import { CancellationToken, CodeActionContext, CodeActionProvider, Command, Rang
  */
 @injectable()
 export class TypescriptCodeActionProvider implements CodeActionProvider {
+    private logger: Logger;
 
+    constructor(
+        @inject('LoggerFactory') loggerFactory: LoggerFactory,
+        private resolveIndex: ResolveIndex
+    ) {
+        this.logger = loggerFactory('TypescriptCodeActionProvider');
+    }
 
     /**
      * Provides the commands to execute for a given problem.
@@ -31,6 +49,54 @@ export class TypescriptCodeActionProvider implements CodeActionProvider {
         context: CodeActionContext,
         token: CancellationToken
     ): Command[] | Thenable<Command[]> {
-        return null;
+        let commands = [],
+            match: RegExpExecArray;
+
+        for (let diagnostic of context.diagnostics) {
+            switch (true) {
+                // When the problem is a missing import, add the import to the document.
+                case !!(match = isMissingImport(diagnostic)):
+                    let info = this.resolveIndex.declarationInfos.find(o => o.declaration.name === match[1]);
+                    if (info) {
+                        commands.push(this.createCommand(
+                            `Import ${info.declaration.name} to the document.`,
+                            new AddImportCodeAction(document, info)
+                        ));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return commands;
     }
+
+    /**
+     * Creates a customized command for the lightbulb with the correct strings.
+     * 
+     * @private
+     * @param {string} title
+     * @param {CodeAction} codeAction
+     * @returns {Command}
+     * 
+     * @memberOf TypescriptCodeActionProvider
+     */
+    private createCommand(title: string, codeAction: CodeAction): Command {
+        return {
+            arguments: [codeAction],
+            command: 'typescriptHero.codeFix.executeCodeAction',
+            title
+        };
+    }
+}
+
+/**
+ * Determines if the problem is a missing import.
+ * 
+ * @param {Diagnostic} diagnostic
+ * @returns {RegExpExecArray}
+ */
+function isMissingImport(diagnostic: Diagnostic): RegExpExecArray {
+    return /cannot find name ['"](.*)['"]/ig.exec(diagnostic.message);
 }
