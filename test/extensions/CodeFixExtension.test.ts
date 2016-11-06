7586d17060cb31d599c02421c11fafa12e9f46c2
Christoph Bühler <christoph@smartive.ch>
2016-11-06 13:23:30 +0100

    code fix tests

--- test/extensions/CodeFixExtension.test.ts
+++ test/extensions/CodeFixExtension.test.ts
@@ -1,45 +1,96 @@
 import { CodeFixExtension } from '../../src/extensions/CodeFixExtension';
 import { Injector } from '../../src/IoC';
+import { CodeAction } from '../../src/models/CodeAction';
+import { TypescriptCodeActionProvider } from '../../src/provider/TypescriptCodeActionProvider';
 import * as chai from 'chai';
 import { join } from 'path';
-import * as vscode from 'vscode';
+import * as sinon from 'Sinon';
+import { Position, Range, TextDocument, window, workspace } from 'vscode';
 
 chai.should();
 
+class SpyCodeAction implements CodeAction {
+    constructor(private spy: sinon.SinonSpy, private result: boolean) { }
+
+    public async execute(): Promise<boolean> {
+        this.spy();
+        return this.result;
+    }
+}
+
 describe('CodeFixExtension', () => {
 
-    let extension: any;
+    let extension: any,
+        actionProvider: TypescriptCodeActionProvider;
 
     before(() => {
         extension = Injector.getAll<CodeFixExtension>('Extension').find(o => o instanceof CodeFixExtension);
+        actionProvider = Injector.get<TypescriptCodeActionProvider>(TypescriptCodeActionProvider);
     });
 
     describe('executeCodeAction', () => {
 
-        // const file = join(vscode.workspace.rootPath, 'resolveExtension/addImportToDocument.ts');
-        // let document: vscode.TextDocument;
+        const file = join(workspace.rootPath, 'codeFixExtension/empty.ts');
+        let document: TextDocument;
 
-        // before(async done => {
-        //     document = await vscode.workspace.openTextDocument(file);
-        //     await vscode.window.showTextDocument(document);
-        //     done();
-        // });
+        before(async done => {
+            document = await workspace.openTextDocument(file);
+            await window.showTextDocument(document);
+            done();
+        });
 
-        // afterEach(async done => {
-        //     await vscode.window.activeTextEditor.edit(builder => {
-        //         builder.delete(new vscode.Range(
-        //             new vscode.Position(0, 0),
-        //             document.lineAt(document.lineCount - 1).rangeIncludingLineBreak.end
-        //         ));
-        //     });
-        //     done();
-        // });
+        afterEach(async done => {
+            await window.activeTextEditor.edit(builder => {
+                builder.delete(new Range(
+                    new Position(0, 0),
+                    document.lineAt(document.lineCount - 1).rangeIncludingLineBreak.end
+                ));
+            });
+            done();
+        });
 
-        it('should call the execute method of a code action');
+        it('should call the execute method of a code action', async done => {
+            try {
+                let spy = sinon.spy();
+                await extension.executeCodeAction(new SpyCodeAction(spy, true));
+                spy.should.be.calledOnce;
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
-        it('should add a missing import to a document');
+        it('should add a missing import to a document', async done => {
+            try {
+                let cmds = actionProvider.provideCodeActions(
+                    document,
+                    new Range(new Position(0, 0), new Position(0, 0)),
+                    <any>{ diagnostics: [{ message: `Cannot find name 'Class1'.` }] },
+                    null
+                );
+                await extension.executeCodeAction(cmds[0].arguments[0]);
+                document.lineAt(0).text.should.equal(`import { Class1 } from '../resourceIndex';`);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
-        it('should warn the user if the result is false');
+        it('should warn the user if the result is false', async done => {
+            let stub = sinon.stub(window, 'showWarningMessage', (text) => {
+                return Promise.resolve();
+            });
+
+            try {
+                await extension.executeCodeAction(new SpyCodeAction(sinon.spy(), false));
+                stub.should.be.calledWith('The provided code action could not complete. Please see the logs.');
+                done();
+            } catch (e) {
+                done(e);
+            } finally {
+                stub.restore();
+            }
+        });
 
     });
 
