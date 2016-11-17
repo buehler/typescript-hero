5a989e7051befe07d123c36eaad45adfe6da4cb2
Christoph Bühler <christoph@smartive.ch>
2016-11-17 08:20:58 +0100

    add testfile for import proxy

---
+++ test/models/ImportProxy.test.ts
@@ -0,0 +1,11 @@
+import { ResolveExtension } from '../../src/extensions/ResolveExtension';
+import { Injector } from '../../src/IoC';
+import * as chai from 'chai';
+import { join } from 'path';
+import * as vscode from 'vscode';
+
+chai.should();
+
+describe('ImportProxy', () => {
+
+});
