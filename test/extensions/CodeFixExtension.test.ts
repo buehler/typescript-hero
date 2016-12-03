360a10eb8a6b09327f6fb43c735e5976fbf06cf4
Christoph Bühler <christoph@smartive.ch>
2016-12-03 15:04:33 +0100

    coding fix extension tests

--- test/extensions/CodeFixExtension.test.ts
+++ test/extensions/CodeFixExtension.test.ts
@@ -18,7 +18,7 @@ class SpyCodeAction implements CodeAction {
     }
 }
 
-describe('CodeFixExtension', () => {
+describe.only('CodeFixExtension', () => {
 
     let extension: any,
         actionProvider: TypescriptCodeActionProvider;
@@ -141,13 +141,99 @@ describe('CodeFixExtension', () => {
             done();
         });
 
-        it('should add interface elements to a class');
+        it('should add interface elements to a class', async done => {
+            try {
+                let cmds = await actionProvider.provideCodeActions(
+                    document,
+                    new Range(new Position(0, 0), new Position(0, 0)),
+                    <any>{ diagnostics: [{ message: `class 'InterfaceImplement' incorrectly implements 'CodeFixImplementInterface'.` }] },
+                    null
+                );
+                await extension.executeCodeAction(cmds[0].arguments[0]);
+                document.lineAt(3).text.should.equal(`class InterfaceImplement implements CodeFixImplementInterface {`);
+                document.lineAt(4).text.should.equal(`    public property: number;`);
+                document.lineAt(6).text.should.equal(`    public interfaceMethod(): string {`);
+                document.lineAt(7).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(8).text.should.equal(`    }`);
+                document.lineAt(10).text.should.equal(`    public interfaceMethodWithParams(p1: string, p2): number {`);
+                document.lineAt(11).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(12).text.should.equal(`    }`);
+                document.lineAt(13).text.should.equal(`}`);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
-        it('should add abstract class elements to a class');
+        it('should add abstract class elements to a class', async done => {
+            try {
+                let cmds = await actionProvider.provideCodeActions(
+                    document,
+                    new Range(new Position(0, 0), new Position(0, 0)),
+                    <any>{ diagnostics: [{ message: `non-abstract class 'AbstractImplement' implement inherited from class 'CodeFixImplementAbstract'.` }] },
+                    null
+                );
+                await extension.executeCodeAction(cmds[0].arguments[0]);
+                document.lineAt(6).text.should.equal(`class AbstractImplement extends CodeFixImplementAbstract {`);
+                document.lineAt(7).text.should.equal(`    public pubProperty: string;`);
+                document.lineAt(9).text.should.equal(`    public abstractMethod(): void {`);
+                document.lineAt(10).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(11).text.should.equal(`    }`);
+                document.lineAt(13).text.should.equal(`    public abstractMethodWithParams(p1: string, p2): number {`);
+                document.lineAt(14).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(15).text.should.equal(`    }`);
+                document.lineAt(16).text.should.equal(`}`);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
-        it('should add local interface elements to a class');
+        it('should add local interface elements to a class', async done => {
+            try {
+                let cmds = await actionProvider.provideCodeActions(
+                    document,
+                    new Range(new Position(0, 0), new Position(0, 0)),
+                    <any>{ diagnostics: [{ message: `class 'InternalInterfaceImplement' incorrectly implements 'InternalInterface'.` }] },
+                    null
+                );
+                await extension.executeCodeAction(cmds[0].arguments[0]);
+                document.lineAt(19).text.should.equal(`class InternalInterfaceImplement implements InternalInterface {`);
+                document.lineAt(21).text.should.equal(`    public method(p1: string): void {`);
+                document.lineAt(22).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(23).text.should.equal(`    }`);
+                document.lineAt(25).text.should.equal(`    public methodTwo() {`);
+                document.lineAt(26).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(27).text.should.equal(`    }`);
+                document.lineAt(28).text.should.equal(`}`);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
-        it('should add local abstract class elements to a class');
+        it('should add local abstract class elements to a class', async done => {
+            try {
+                let cmds = await actionProvider.provideCodeActions(
+                    document,
+                    new Range(new Position(0, 0), new Position(0, 0)),
+                    <any>{ diagnostics: [{ message: `non-abstract class 'InternalAbstractImplement' implement inherited from class 'InternalAbstract'.` }] },
+                    null
+                );
+                await extension.executeCodeAction(cmds[0].arguments[0]);
+                document.lineAt(22).text.should.equal(`class InternalAbstractImplement extends InternalAbstract {`);
+                document.lineAt(24).text.should.equal(`    public method(): void {`);
+                document.lineAt(25).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(26).text.should.equal(`    }`);
+                document.lineAt(28).text.should.equal(`    public abstractMethod(): void {`);
+                document.lineAt(29).text.should.equal(`        throw new Error('Not implemented yet.');`);
+                document.lineAt(30).text.should.equal(`    }`);
+                document.lineAt(31).text.should.equal(`}`);
+                done();
+            } catch (e) {
+                done(e);
+            }
+        });
 
     });
 
