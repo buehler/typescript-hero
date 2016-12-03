0e0c54c204c93a8358a8bab11480e7fc46784a84
Christoph Bühler <christoph@smartive.ch>
2016-12-03 13:46:56 +0100

    adding test classes

--- test/_workspace/codeFixExtension/implementInterfaceOrAbstract.ts
+++ test/_workspace/codeFixExtension/implementInterfaceOrAbstract.ts
@@ -4,3 +4,25 @@ import { CodeFixImplementInterface } from './exportedObjects';
 class InterfaceImplement implements CodeFixImplementInterface {
 
 }
+
+class AbstractImplement {
+
+}
+
+abstract class InternalAbstract {
+    public method(): void{}
+    public abstract abstractMethod(): void;
+}
+
+interface InternalInterface {
+    method(p1: string): void;
+    methodTwo();
+}
+
+class InternalInterfaceImplement {
+
+}
+
+class InternalAbstractImplement extends InternalAbstract {
+
+}
