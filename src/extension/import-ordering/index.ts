b406b74ef99df78d722cc72facc2b37eacbb9ec4
Christoph Bühler <christoph@smartive.ch>
2017-05-31 23:49:21 +0200

    add barrel file

---
+++ src/extension/import-ordering/index.ts
@@ -0,0 +1,6 @@
+export * from './ImportGroup';
+export * from './ImportGroupKeyword';
+export * from './ImportGroupOrder';
+export * from './KeywordImportGroup';
+export * from './RegexImportGroup';
+export * from './RemainImportGroup';
