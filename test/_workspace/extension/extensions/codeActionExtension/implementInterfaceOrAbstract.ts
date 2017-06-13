c9393d98142848a8cc7a9945e9cf1930209340c5
Christoph Bühler <christoph@smartive.ch>
2017-06-13 07:43:07 +0200

    extends file with new test cases

--- test/_workspace/extension/extensions/codeActionExtension/implementInterfaceOrAbstract.ts
+++ test/_workspace/extension/extensions/codeActionExtension/implementInterfaceOrAbstract.ts
@@ -8,7 +8,7 @@ class AbstractImplement extends CodeFixImplementAbstract {
 }
 
 abstract class InternalAbstract {
-    public method(): void{}
+    public method(): void { }
     public abstract abstractMethod(): void;
 }
 
@@ -21,4 +21,19 @@ class InternalInterfaceImplement implements InternalInterface {
 }
 
 class InternalAbstractImplement extends InternalAbstract {
+}
+
+interface GenericInterface<T1, T2> {
+    method(p1: T1): T2;
+}
+
+abstract class GenericAbstractClass<T1, T2, T3> {
+    public abstract abstractMethod(p1: T1): T2;
+    protected abstract protMethod(p2: T2, p3: T3);
+}
+
+class ImplementGenericInterface implements GenericInterface<string, number> {
+}
+
+class ImplementGenericAbstract extends GenericAbstractClass<string, number, boolean> {
 }
