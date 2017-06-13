98717a05f6cd64eed1e8a85395548d0dee0c8913
Christoph Bühler <christoph@smartive.ch>
2017-06-13 07:31:29 +0200

    adding test for generic type parsing

--- test/_workspace/common/ts-parsing/interface.ts
+++ test/_workspace/common/ts-parsing/interface.ts
@@ -8,6 +8,9 @@ interface NonExportedInterface {
 export interface ExportedInterface {
     property1: string;
     property2: number;
-    method1({param1, param2});
+    method1({ param1, param2 });
     method2([param1, param2]): void;
 }
+
+interface Generic<T> { }
+interface MultiGeneric<TIn, TOut, TError> { }
