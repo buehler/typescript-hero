5e2309cec415d5ba558545eabed5e1041603dadd
Christoph Bühler <christoph@smartive.ch>
2017-06-16 16:59:17 +0200

    adding import specifier tree items

--- src/extension/provider-items/document-structure/ImportsStructureTreeItem.ts
+++ src/extension/provider-items/document-structure/ImportsStructureTreeItem.ts
@@ -1,8 +1,30 @@
 import { ExtensionContext, TreeItemCollapsibleState } from 'vscode';
 
-import { Import } from '../../../common/ts-parsing/imports';
+import {
+    DefaultImport,
+    ExternalModuleImport,
+    Import,
+    NamedImport,
+    NamespaceImport,
+    StringImport,
+} from '../../../common/ts-parsing/imports';
 import { File } from '../../../common/ts-parsing/resources';
 import { BaseStructureTreeItem } from './BaseStructureTreeItem';
+
+/**
+ * Import specifier tree item that represents a specific (named) import of an import statement.
+ * 
+ * @export
+ * @class ImportSpecifierStructureTreeItem
+ * @extends {BaseStructureTreeItem}
+ */
+export class ImportSpecifierStructureTreeItem extends BaseStructureTreeItem {
+    constructor(name: string, tsImport: Import, context: ExtensionContext) {
+        super(name);
+        this.iconPath = context.asAbsolutePath('./src/extension/assets/icons/declarations/default.svg');
+        this.command = this.createJumpToCommand([tsImport]);
+    }
+}
 
 /**
  * Structure item that represents an import in a file.
@@ -12,10 +34,34 @@ import { BaseStructureTreeItem } from './BaseStructureTreeItem';
  * @extends {BaseStructureTreeItem}
  */
 export class ImportStructureTreeItem extends BaseStructureTreeItem {
-    constructor(tsImport: Import, context: ExtensionContext) {
+    constructor(private tsImport: Import, private context: ExtensionContext) {
         super(tsImport.libraryName);
         this.iconPath = context.asAbsolutePath('./src/extension/assets/icons/declarations/import.svg');
         this.command = this.createJumpToCommand([tsImport]);
+
+        if (!(tsImport instanceof StringImport)) {
+            this.collapsibleState = TreeItemCollapsibleState.Collapsed;
+        }
+    }
+
+    public getChildren(): BaseStructureTreeItem[] {
+        const imp = this.tsImport;
+        if (imp instanceof DefaultImport) {
+            return [new ImportSpecifierStructureTreeItem(imp.alias, imp, this.context)];
+        } else if (imp instanceof ExternalModuleImport) {
+            return [new ImportSpecifierStructureTreeItem(imp.alias, imp, this.context)];
+        } else if (imp instanceof NamedImport) {
+            return imp.specifiers.map(
+                s => new ImportSpecifierStructureTreeItem(
+                    `${s.specifier}${s.alias ? ` as ${s.alias}` : ''}`,
+                    imp,
+                    this.context,
+                ),
+            );
+        } else if (imp instanceof NamespaceImport) {
+            return [new ImportSpecifierStructureTreeItem(imp.alias, imp, this.context)];
+        }
+        return [];
     }
 }
 
