217a45c871fc3b1ebbf3a6ad58263ee6fbfa37c1
Christoph Bühler <christoph@smartive.ch>
2017-06-20 07:56:39 +0200

    add tests for complex regex parsing

--- test/extension/import-grouping/ImportGroupSettingParser.test.ts
+++ test/extension/import-grouping/ImportGroupSettingParser.test.ts
@@ -9,7 +9,7 @@ import * as chai from 'chai';
 
 chai.should();
 
-describe('ImportGroupSettingParser', () => {
+describe.only('ImportGroupSettingParser', () => {
 
     it('should parse a simple keyword', () => {
         const result = ImportGroupSettingParser.parseSetting('Workspace') as KeywordImportGroup;
@@ -49,6 +49,27 @@ describe('ImportGroupSettingParser', () => {
         const fn = () => ImportGroupSettingParser.parseSetting('whatever');
 
         fn.should.throw(ImportGroupIdentifierInvalidError);
+    });
+
+    it('should parse a regex with "or"', () => {
+        const result = ImportGroupSettingParser.parseSetting('/angular|react/');
+
+        result.should.be.an.instanceof(RegexImportGroup);
+        (result as RegexImportGroup).regex.should.equal('/angular|react/');
+    });
+
+    it('should parse a regex with "@"', () => {
+        const result = ImportGroupSettingParser.parseSetting('/@angular/');
+
+        result.should.be.an.instanceof(RegexImportGroup);
+        (result as RegexImportGroup).regex.should.equal('/@angular/');
+    });
+
+    it('should parse a complex regex', () => {
+        const result = ImportGroupSettingParser.parseSetting('/(@angular|react)/core/(.*)/');
+
+        result.should.be.an.instanceof(RegexImportGroup);
+        (result as RegexImportGroup).regex.should.equal('/(@angular|react)/core/(.*)/');
     });
 
 });
