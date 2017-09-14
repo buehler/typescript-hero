d9fefa7a36a3864e1e8901f4557ab649a7707359
Christoph Bühler <christoph.buehler@bluewin.ch>
2017-09-14 21:45:28 +0200

    add extension part

---
+++ src/extension/extensions/OrganizeImportsOnSaveExtension.ts
@@ -0,0 +1,59 @@
+import { inject, injectable } from 'inversify';
+import { ExtensionContext, workspace } from 'vscode';
+
+import { ExtensionConfig } from '../../common/config';
+import { Logger, LoggerFactory } from '../../common/utilities';
+import { iocSymbols } from '../IoCSymbols';
+import { ImportManager } from '../managers';
+import { BaseExtension } from './BaseExtension';
+
+/**
+ * 
+ * 
+ * @export
+ * @class OrganizeImportsOnSaveExtension
+ * @extends {BaseExtension}
+ */
+@injectable()
+export class OrganizeImportsOnSaveExtension extends BaseExtension {
+    private logger: Logger;
+
+    constructor(
+        @inject(iocSymbols.extensionContext) context: ExtensionContext,
+        @inject(iocSymbols.loggerFactory) loggerFactory: LoggerFactory,
+        @inject(iocSymbols.configuration) private config: ExtensionConfig,
+    ) {
+        super(context);
+        this.logger = loggerFactory('ImportResolveExtension');
+    }
+
+    /**
+     * Initialized the extension. Registers the commands and other disposables to the context.
+     * 
+     * @memberof OrganizeImportsOnSaveExtension
+     */
+    public initialize(): void {
+        this.context.subscriptions.push(workspace.onWillSaveTextDocument((event) => {
+            if (!this.config.resolver.organizeOnSave) {
+                this.logger.info('Organize on save is deactivated through config.');
+                return;
+            }
+
+            event.waitUntil(
+                ImportManager.create(event.document)
+                    .then(manager => manager.organizeImports().calculateTextEdits()),
+            );
+        }));
+
+        this.logger.info('Initialized');
+    }
+
+    /**
+     * Disposes the extension.
+     * 
+     * @memberof OrganizeImportsOnSaveExtension
+     */
+    public dispose(): void {
+        this.logger.info('Disposed');
+    }
+}
