42a2323830773f1f0ac37c7c7f9634058c39a53e
Christoph Bühler <christoph@smartive.ch>
2017-09-18 08:23:21 +0200

    add impl

--- src/common/helpers/ImportHelpers.ts
+++ src/common/helpers/ImportHelpers.ts
@@ -3,7 +3,7 @@ import { Position, TextEditor } from 'vscode';
 /**
  * Calculate the position, where a new import should be inserted.
  * Does respect the "use strict" string as first line of a document.
- * 
+ *
  * @export
  * @param {TextEditor | undefined} editor
  * @returns {Position}
@@ -12,5 +12,7 @@ export function getImportInsertPosition(editor: TextEditor | undefined): Positio
     if (!editor) {
         return new Position(0, 0);
     }
-    return editor.document.lineAt(0).text.match(/use strict/) ? new Position(1, 0) : new Position(0, 0);
+    const lines = editor.document.getText().split('\n');
+    const index = lines.findIndex(line => !line.match(/^\s*(\/\/|\/\*\*|\*\/|\*|['"]use strict['"])/g));
+    return new Position(Math.max(0, index), 0);
 }
