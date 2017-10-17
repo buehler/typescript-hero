db174f72a6b72845b5d5e23818e0228477249587
Christoph Bühler <christoph@smartive.ch>
2017-10-17 07:49:59 +0200

    change the config interface

--- src/common/config/CodeOutlineConfig.ts
+++ src/common/config/CodeOutlineConfig.ts
@@ -1,13 +1,25 @@
+import { Uri } from 'vscode';
+
 /**
  * Configuration interface for the code outline feature.
- * 
+ *
  * @export
  * @interface CodeOutlineConfig
  */
 export interface CodeOutlineConfig {
     /**
+     * The given resource URI (if any) for the actual configuration.
+     * Is needed to determine the actual config values for multi root environments.
+     *
+     * @readonly
+     * @type {(Uri | undefined)}
+     * @memberof CodeOutlineConfig
+     */
+    resource: Uri | undefined;
+
+    /**
      * Defined if the code outline feature is enabled or not.
-     * 
+     *
      * @type {boolean}
      * @memberof CodeOutlineConfig
      */
--- src/common/config/ExtensionConfig.ts
+++ src/common/config/ExtensionConfig.ts
@@ -1,17 +1,29 @@
+import { Uri } from 'vscode';
+
 import { CodeOutlineConfig } from './CodeOutlineConfig';
 import { ResolverConfig } from './ResolverConfig';
 
 /**
  * Configuration interface for TypeScript Hero
  * Contains all exposed config endpoints.
- * 
+ *
  * @export
  * @interface ExtensionConfig
  */
 export interface ExtensionConfig {
     /**
+     * The given resource URI (if any) for the actual configuration.
+     * Is needed to determine the actual config values for multi root environments.
+     *
+     * @readonly
+     * @type {(Uri | undefined)}
+     * @memberof ExtensionConfig
+     */
+    resource: Uri | undefined;
+
+    /**
      * The actual log level.
-     * 
+     *
      * @readonly
      * @type {string}
      * @memberof ExtensionConfig
@@ -20,7 +32,7 @@ export interface ExtensionConfig {
 
     /**
      * Configuration object for the resolver extension.
-     * 
+     *
      * @readonly
      * @type {ResolverConfig}
      * @memberof ExtensionConfig
@@ -29,7 +41,7 @@ export interface ExtensionConfig {
 
     /**
     * Configuration object for the code outline extension.
-    * 
+    *
     * @readonly
     * @type {CodeOutlineConfig}
     * @memberof ExtensionConfig
@@ -40,7 +52,7 @@ export interface ExtensionConfig {
      * Completion sorting mode:
      *  default: Use default VSCode sorting mode
      *  bottom: Push to bottom
-     * 
+     *
      * @readonly
      * @type {'default'|'bottom'}
      * @memberof ExtensionConfig
