653abe5ba4c92f2d6a4fc221789323deadbfc0a0
Christoph Bühler <christoph@smartive.ch>
2017-10-27 11:43:09 +0200

    adding winston logs

--- src/extension/utilities/winstonLogger.ts
+++ src/extension/utilities/winstonLogger.ts
@@ -1,6 +1,7 @@
-import { ExtensionContext } from 'vscode';
+import { join } from 'path';
+import { ExtensionContext, OutputChannel, window, workspace } from 'vscode';
 
-const { createLogger, format, transports } = require('winston');
+const { createLogger, exceptions, format, transports } = require('winston');
 const transport = require('winston-transport');
 const { LEVEL, MESSAGE } = require('triple-beam');
 
@@ -15,7 +16,7 @@ const levels = {
 };
 
 class OutputWindowTransport extends Transport {
-    constructor(opts?: any) {
+    constructor(opts: any, private channel: OutputChannel) {
         super(opts);
     }
 
@@ -23,19 +24,7 @@ class OutputWindowTransport extends Transport {
         setImmediate(() => {
             this.emit('logged', info);
         });
-        const level = info[LEVEL];
-
-        switch (level) {
-            case 'error':
-                console.error(info[MESSAGE]);
-                break;
-            case 'warn':
-                console.warn(info[MESSAGE]);
-                break;
-            default:
-                console.log(info[MESSAGE]);
-                break;
-        }
+        this.channel.appendLine(info[MESSAGE]);
         callback();
     }
 }
@@ -66,11 +55,38 @@ class ConsoleLogTransport extends Transport {
     }
 }
 
+class HandleUncatchedException extends Transport {
+    constructor(private path: string) {
+        super();
+    }
+
+    public async log(info: any, callback: any): Promise<void> {
+        setImmediate(() => {
+            this.emit('logged', info);
+        });
+
+        const result = await window.showErrorMessage(
+            'There was an uncought exception, do you want to see the logfile?',
+            { modal: true },
+            'Yes, show me.',
+        );
+
+        if (result) {
+            const doc = await workspace.openTextDocument(join(this.path, 'typescript-hero.log'));
+            await window.showTextDocument(doc);
+        }
+
+        callback();
+    }
+}
+
 export interface Logger {
     error: (message: string, ...data: any[]) => void;
     warn: (message: string, ...data: any[]) => void;
     info: (message: string, ...data: any[]) => void;
     debug: (message: string, ...data: any[]) => void;
+    profile: (name: string) => void;
+    startTimer(): { done: (info: { message: string }) => void; };
 }
 
 const loggerTransports = [
@@ -83,10 +99,28 @@ export default function winstonLogger(verbosity: keyof typeof levels, context: E
     const level = !!process.env.CI ? 'error' : verbosity;
 
     if (!process.env.CI && !process.env.EXT_DEBUG) {
-        loggerTransports.push(new OutputWindowTransport());
+        const channel = window.createOutputChannel('TypeScript Hero');
+        context.subscriptions.push(channel);
+
+        const fileHandler = new transports.File({
+            exitOnError: false,
+            filename: 'typescript-hero.log',
+            dirname: context.extensionPath,
+            maxsize: 1024 * 1024,
+            maxFiles: 1,
+            tailable: true,
+        });
+        const outputHandler = new OutputWindowTransport({ exitOnError: false }, channel);
+
+        loggerTransports.push(fileHandler);
+        loggerTransports.push(outputHandler);
+
+        exceptions.handle(fileHandler);
+        exceptions.handle(outputHandler);
+        exceptions.handle(new HandleUncatchedException(context.extensionPath));
     }
 
-    return createLogger({
+    const logger = createLogger({
         level,
         levels,
         format: combine(
@@ -108,5 +142,9 @@ export default function winstonLogger(verbosity: keyof typeof levels, context: E
             }),
         ),
         transports: loggerTransports,
-    }) as Logger;
+    });
+
+    logger.exitOnError = false;
+
+    return logger;
 }
