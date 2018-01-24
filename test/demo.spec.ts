eb935d76564b797abad68cde5dfe4cf5096a32d5
Christoph Bühler <christoph@smartive.ch>
2018-01-24 08:27:30 +0100

    change to mocha

--- test/demo.spec.ts
+++ test/demo.spec.ts
@@ -1,5 +1,9 @@
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
 describe('Demo test', () => {
   it('should be tested', () => {
-    expect(true).toBe(true);
+    expect(true).to.equal(true);
   });
 });
