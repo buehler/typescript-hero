bb55f4859ab3a03a5ef0bc07603e5acb30dde8aa
Christoph Bühler <christoph@smartive.ch>
2018-02-07 11:09:02 +0100

    remove consolelog

--- test/tests/setup.ts
+++ test/tests/setup.ts
@@ -19,7 +19,6 @@ beforeEach(function (): void {
   const fileFromTestRoot = ((this.currentTest as any).file.replace(/.*out[\\/]/, '')).replace(/\.js$/, '.ts');
   const tsFile = parse(join(global['rootPath'], fileFromTestRoot));
   const snapPath = join(tsFile.dir, '__snapshots__', tsFile.base);
-  console.log('snap path before all', snapPath);
   chaiJestSnapshot.configureUsingMochaContext(this);
   chaiJestSnapshot.setFilename(`${snapPath}.snap`);
 });
