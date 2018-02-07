3af791ce4a07ee6a1de433906511b0eeb9651a85
Christoph Bühler <christoph@smartive.ch>
2018-02-07 11:04:54 +0100

    regex

--- test/tests/setup.ts
+++ test/tests/setup.ts
@@ -16,7 +16,7 @@ use(chaiJestSnapshot);
 before(() => chaiJestSnapshot.resetSnapshotRegistry());
 
 beforeEach(function (): void {
-  const fileFromTestRoot = ((this.currentTest as any).file.replace(/.*out[\/|\\]/, '')).replace(/[.]js$/, '.ts');
+  const fileFromTestRoot = ((this.currentTest as any).file.replace(/.*out[\\/]/, '')).replace(/\.js$/, '.ts');
   const tsFile = parse(join(global['rootPath'], fileFromTestRoot));
   const snapPath = join(tsFile.dir, '__snapshots__', tsFile.base);
   console.log('snap path before all', snapPath);
