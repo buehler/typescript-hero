95faa54a8f518012e2ab7528ce3110a8b8603a8b
Christoph Bühler <christoph@smartive.ch>
2018-02-12 16:45:29 +0100

    tests for regex grp

--- test/tests/imports/import-grouping/regex-import-group.test.ts
+++ test/tests/imports/import-grouping/regex-import-group.test.ts
@@ -1,102 +1,109 @@
-// import * as chai from 'chai';
-// import { join } from 'path';
-// import { File, NamedImport, TypescriptCodeGenerator, TypescriptParser } from 'typescript-parser';
-// import { workspace } from 'vscode';
+import { join } from 'path';
+import {
+  File,
+  Generatable,
+  GENERATORS,
+  NamedImport,
+  TypescriptCodeGenerator,
+  TypescriptGenerationOptions,
+  TypescriptParser,
+} from 'typescript-parser';
+import { Uri, workspace } from 'vscode';
 
-// import { TypescriptCodeGeneratorFactory } from '../../../../src/common/factories';
-// import { RegexImportGroup } from '../../../../src/extension/import-grouping';
-// import { Container } from '../../../../src/extension/IoC';
-// import { iocSymbols } from '../../../../src/extension/IoCSymbols';
+import { RegexImportGroup } from '../../../../src/imports/import-grouping';
+import ioc from '../../../../src/ioc';
+import iocSymbols, { TypescriptCodeGeneratorFactory } from '../../../../src/ioc-symbols';
+import { expect } from '../../setup';
 
-// chai.should();
+describe('RegexImportGroup', () => {
 
-// describe('RegexImportGroup', () => {
+  const rootPath = workspace.workspaceFolders![0].uri.fsPath;
+  const fsFile = Uri.file(
+    join(rootPath, 'imports', 'import-grouping', 'imports.ts'),
+  );
+  let file: File;
+  let importGroup: RegexImportGroup;
+  let generator: TypescriptCodeGenerator;
 
-//     const rootPath = workspace.workspaceFolders![0].uri.fsPath;
-//     let file: File;
-//     let importGroup: RegexImportGroup;
-//     let generator: TypescriptCodeGenerator;
+  before(() => {
+    if (!GENERATORS[RegexImportGroup.name]) {
+      GENERATORS[RegexImportGroup.name] = (generatable: Generatable, options: TypescriptGenerationOptions): string => {
+        const gen = new TypescriptCodeGenerator(options);
+        const group = generatable as RegexImportGroup;
+        if (!group.imports.length) {
+          return '';
+        }
+        return group.sortedImports
+          .map(imp => gen.generate(imp))
+          .join('\n') + '\n';
+      };
+    }
+  });
 
-//     before(async () => {
-//         const parser = Container.get<TypescriptParser>(iocSymbols.typescriptParser);
-//         generator = Container.get<TypescriptCodeGeneratorFactory>(iocSymbols.generatorFactory)();
-//         file = await parser.parseFile(
-//             join(
-//                 rootPath,
-//                 'extension/import-grouping/imports.ts',
-//             ),
-//             rootPath,
-//         );
-//     });
+  before(async () => {
+    const parser = ioc.get<TypescriptParser>(iocSymbols.parser);
+    generator = ioc.get<TypescriptCodeGeneratorFactory>(iocSymbols.generatorFactory)(fsFile);
+    file = await parser.parseFile(fsFile.fsPath, rootPath);
+  });
 
-//     beforeEach(() => {
-//         importGroup = new RegexImportGroup(`/Lib/`);
-//     });
+  beforeEach(() => {
+    importGroup = new RegexImportGroup(`/Lib/`);
+  });
 
-//     it('should process a matching import', () => {
-//         importGroup.processImport(file.imports[0]).should.be.true;
-//     });
+  it('should process a matching import', () => {
+    expect(importGroup.processImport(file.imports[0])).to.be.true;
+  });
 
-//     it('should not process a not matching import', () => {
-//         importGroup.processImport(file.imports[2]).should.be.false;
-//     });
+  it('should not process a not matching import', () => {
+    expect(importGroup.processImport(file.imports[2])).to.be.false;
+  });
 
-//     it('should correctly process a list of imports', () => {
-//         file.imports.map(i => importGroup.processImport(i)).should.deep.equal([true, true, false, false, true, true]);
-//     });
+  it('should correctly process a list of imports', () => {
+    expect(file.imports.map(i => importGroup.processImport(i))).to.matchSnapshot();
+  });
 
-//     it('should generate the correct typescript (asc)', () => {
-//         for (const imp of file.imports) {
-//             if (importGroup.processImport(imp)) {
-//                 continue;
-//             }
-//         }
-//         generator.generate(importGroup as any).should.equal(
-//             `import './workspaceSideEffectLib';\n` +
-//             `import 'sideEffectLib';\n` +
-//             `import { AnotherModuleFoo } from 'anotherLib';\n` +
-//             `import { ModuleFoobar } from 'myLib';\n`,
-//         );
-//     });
+  it('should generate the correct typescript (asc)', () => {
+    for (const imp of file.imports) {
+      if (importGroup.processImport(imp)) {
+        continue;
+      }
+    }
+    expect(generator.generate(importGroup as any)).to.matchSnapshot();
+  });
 
-//     it('should generate the correct typescript (desc)', () => {
-//         (importGroup as any).order = 'desc';
-//         for (const imp of file.imports) {
-//             if (importGroup.processImport(imp)) {
-//                 continue;
-//             }
-//         }
-//         generator.generate(importGroup as any).should.equal(
-//             `import 'sideEffectLib';\n` +
-//             `import './workspaceSideEffectLib';\n` +
-//             `import { ModuleFoobar } from 'myLib';\n` +
-//             `import { AnotherModuleFoo } from 'anotherLib';\n`,
-//         );
-//     });
+  it('should generate the correct typescript (desc)', () => {
+    (importGroup as any).order = 'desc';
+    for (const imp of file.imports) {
+      if (importGroup.processImport(imp)) {
+        continue;
+      }
+    }
+    expect(generator.generate(importGroup as any)).to.matchSnapshot();
+  });
 
-//     it('should work with regex "or" conditions', () => {
-//         const group = new RegexImportGroup('/angular|react/');
-//         const imp = new NamedImport('@angular');
-//         const imp2 = new NamedImport('@react/core');
+  it('should work with regex "or" conditions', () => {
+    const group = new RegexImportGroup('/angular|react/');
+    const imp = new NamedImport('@angular');
+    const imp2 = new NamedImport('@react/core');
 
-//         group.processImport(imp).should.be.true;
-//         group.processImport(imp2).should.be.true;
-//     });
+    expect(group.processImport(imp)).to.be.true;
+    expect(group.processImport(imp2)).to.be.true;
+  });
 
-//     it('should work with regex containing an "@"', () => {
-//         const group = new RegexImportGroup('/@angular/');
-//         const imp = new NamedImport('@angular');
+  it('should work with regex containing an "@"', () => {
+    const group = new RegexImportGroup('/@angular/');
+    const imp = new NamedImport('@angular');
 
-//         group.processImport(imp).should.be.true;
-//     });
+    expect(group.processImport(imp)).to.be.true;
+  });
 
-//     it('should work with slash separated regex', () => {
-//         const group = new RegexImportGroup('/@angular/http/');
-//         const imp = new NamedImport('@angular/http');
-//         const imp2 = new NamedImport('@angular/core/component');
+  it('should work with slash separated regex', () => {
+    const group = new RegexImportGroup('/@angular/http/');
+    const imp = new NamedImport('@angular/http');
+    const imp2 = new NamedImport('@angular/core/component');
 
-//         group.processImport(imp).should.be.true;
-//         group.processImport(imp2).should.be.false;
-//     });
+    expect(group.processImport(imp)).to.be.true;
+    expect(group.processImport(imp2)).to.be.false;
+  });
 
-// });
+});
