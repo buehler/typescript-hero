82087e0287bdc96b49ee57bd2e57906d25f435ac
Christoph Bühler <christoph@smartive.ch>
2018-02-12 16:04:21 +0100

    add tests for import group setting parser

--- test/tests/imports/import-grouping/import-group-setting-parser.test.ts
+++ test/tests/imports/import-grouping/import-group-setting-parser.test.ts
@@ -1,66 +1,65 @@
-import { ImportGroupKeyword, ImportGroupSettingParser, KeywordImportGroup } from '../../../../src/imports/import-grouping';
+import {
+  ImportGroupIdentifierInvalidError,
+  ImportGroupSettingParser,
+  KeywordImportGroup,
+  RegexImportGroup,
+} from '../../../../src/imports/import-grouping';
+import { expect } from '../../setup';
 
-describe('ImportGroupSettingParser', () => {
+describe.only('ImportGroupSettingParser', () => {
 
   it('should parse a simple keyword', () => {
     const result = ImportGroupSettingParser.parseSetting('Workspace') as KeywordImportGroup;
 
-    result.keyword.should.equal(ImportGroupKeyword.Workspace);
-    result.order.should.equal('asc');
+    expect(result).to.matchSnapshot();
   });
 
-  // it('should parse a simple regex', () => {
-  //   const result = ImportGroupSettingParser.parseSetting('/foobar/') as RegexImportGroup;
+  it('should parse a simple regex', () => {
+    const result = ImportGroupSettingParser.parseSetting('/foobar/') as RegexImportGroup;
 
-  //   result.regex.should.equal('/foobar/');
-  //   result.order.should.equal('asc');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
-  // it('should parse a complex keyword pattern', () => {
-  //   const result = ImportGroupSettingParser.parseSetting({
-  //     identifier: 'Workspace',
-  //     order: 'desc',
-  //   }) as KeywordImportGroup;
+  it('should parse a complex keyword pattern', () => {
+    const result = ImportGroupSettingParser.parseSetting({
+      identifier: 'Workspace',
+      order: 'desc',
+    }) as KeywordImportGroup;
 
-  //   result.keyword.should.equal(ImportGroupKeyword.Workspace);
-  //   result.order.should.equal('desc');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
-  // it('should parse a complex regex pattern', () => {
-  //   const result = ImportGroupSettingParser.parseSetting({
-  //     identifier: '/foobar/',
-  //     order: 'desc',
-  //   }) as RegexImportGroup;
+  it('should parse a complex regex pattern', () => {
+    const result = ImportGroupSettingParser.parseSetting({
+      identifier: '/foobar/',
+      order: 'desc',
+    }) as RegexImportGroup;
 
-  //   result.regex.should.equal('/foobar/');
-  //   result.order.should.equal('desc');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
-  // it('should throw on non found keyword and regex', () => {
-  //   const fn = () => ImportGroupSettingParser.parseSetting('whatever');
+  it('should throw on non found keyword and regex', () => {
+    const fn = () => ImportGroupSettingParser.parseSetting('whatever');
 
-  //   fn.should.throw(ImportGroupIdentifierInvalidError);
-  // });
+    expect(fn).to.throw(ImportGroupIdentifierInvalidError);
+  });
 
-  // it('should parse a regex with "or"', () => {
-  //   const result = ImportGroupSettingParser.parseSetting('/angular|react/');
+  it('should parse a regex with "or"', () => {
+    const result = ImportGroupSettingParser.parseSetting('/angular|react/');
 
-  //   result.should.be.an.instanceof(RegexImportGroup);
-  //   (result as RegexImportGroup).regex.should.equal('/angular|react/');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
-  // it('should parse a regex with "@"', () => {
-  //   const result = ImportGroupSettingParser.parseSetting('/@angular/');
+  it('should parse a regex with "@"', () => {
+    const result = ImportGroupSettingParser.parseSetting('/@angular/');
 
-  //   result.should.be.an.instanceof(RegexImportGroup);
-  //   (result as RegexImportGroup).regex.should.equal('/@angular/');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
-  // it('should parse a complex regex', () => {
-  //   const result = ImportGroupSettingParser.parseSetting('/(@angular|react)/core/(.*)/');
+  it('should parse a complex regex', () => {
+    const result = ImportGroupSettingParser.parseSetting('/(@angular|react)/core/(.*)/');
 
-  //   result.should.be.an.instanceof(RegexImportGroup);
-  //   (result as RegexImportGroup).regex.should.equal('/(@angular|react)/core/(.*)/');
-  // });
+    expect(result).to.matchSnapshot();
+  });
 
 });
